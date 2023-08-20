7e0bc78ef9eef498cf705dd0f2c9d9ea9557f783
Alex tv <134283578+Bladeh4x@users.noreply.github.com>
2023-08-20 20:19:37 +0100

    Update general.js

--- commands/general.js
+++ commands/general.js
@@ -3,7 +3,7 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : BLADE-MD
+ * @project_name : XLICON-MD
  * @author : salmanytofficial <https://github.com/salmanytofficial>
  * @description : XLICON, A Multi-functional whatsapp bot.
  * @version 0.0.6
