732569ac4dc37fe7f092fbc0a0642176324a8524
Alex tv <134283578+Bladeh4x@users.noreply.github.com>
2023-08-22 17:28:08 +0100

    Update Telegraph.js

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -4,9 +4,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : XLICON-MD
- * @author : SalmanYtOfficial <https://github.com/salmanytofficial>
- * @description : XLICON ,A Multi-functional whatsapp bot.
+ * @project_name : BLADE-MD
+ * @author : SalmanYtOfficial <https://github.com/bladeh4x>
+ * @description : BLADEE ,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 //---------------------------------------------------------------------------
@@ -21,7 +21,7 @@ cmd({
     },
     async(Void, citel,text) => {
  let a = citel.quoted ? citel.quoted.text : citel.text;
-let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=XLICON-MD+Bot&author_name=ProfileCorruptedError &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
+let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=BLADE-MD+Bot&author_name=alextv &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
 return citel.reply(`*Paste created on telegraph*\nName:-${util.format(data.result.title)} \nUrl:- ${util.format(data.result.url)}`)
     }
 );
