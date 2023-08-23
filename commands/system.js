99b4072666ed375199542a312f0ffa0a6d37be2c
Alex tv <134283578+Bladeh4x@users.noreply.github.com>
2023-08-23 11:32:14 +0100

    Update system.js

--- commands/system.js
+++ commands/system.js
@@ -220,7 +220,7 @@ cmd({
             desc: "is bot alive??"
         },
         async(Void, citel, text, isAdmins) => {
-            let alivemessage = Config.ALIVE_MESSAGE || `*A bot made by Team .ʙʟᴀᴅᴇ*`
+            let alivemessage = Config.ALIVE_MESSAGE || `*A bot developed by Team BLADE.*`
             const alivtxt = `
 *Hello, ${citel.pushName},*
 _This is  ${tlang().title}._
