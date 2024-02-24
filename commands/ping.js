f7756a6cae6fd16a1fea1117dca06a99eb1db899
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-02-24 12:22:51 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,27 @@
-const _0x422390=_0x477f;(function(_0x22557c,_0x7aa5d3){const _0x17fcfd=_0x477f,_0x572c6e=_0x22557c();while(!![]){try{const _0x6d368d=parseInt(_0x17fcfd(0xfe))/0x1*(parseInt(_0x17fcfd(0x102))/0x2)+-parseInt(_0x17fcfd(0xfc))/0x3*(parseInt(_0x17fcfd(0x113))/0x4)+parseInt(_0x17fcfd(0x106))/0x5*(-parseInt(_0x17fcfd(0x10d))/0x6)+parseInt(_0x17fcfd(0x119))/0x7+-parseInt(_0x17fcfd(0x114))/0x8+-parseInt(_0x17fcfd(0x105))/0x9*(-parseInt(_0x17fcfd(0x112))/0xa)+-parseInt(_0x17fcfd(0x100))/0xb*(parseInt(_0x17fcfd(0x107))/0xc);if(_0x6d368d===_0x7aa5d3)break;else _0x572c6e['push'](_0x572c6e['shift']());}catch(_0x1e1d54){_0x572c6e['push'](_0x572c6e['shift']());}}}(_0x2a71,0x35a7c));import{exec}from'child_process';function _0x2a71(){const _0x4a4035=['86972qTyKmA','882368KNanox','sender','XLICON-V2','main','\x0aitem1.X-ABLabel:Ponsel\x0aEND:VCARD','2022811jBgdKp','key','9NoEoth','\x20ms','3fuLcFB','split','631180qhznAt','0@s.whatsapp.net','155824RVFwtO','tags','Very Fast⚡\x20⭐ՏTᗩᖇ-ᗰᗪ⭐\x20!\x20Latency:\x20','2106eKJPuk','290910KdYHYB','48xsRGqz','help','neofetch\x20--stdout','toFixed','ping','command','6VcgUba','speed','chat','BEGIN:VCARD\x0aVERSION:3.0\x0aN:;a,;;;\x0aFN:\x27XLICON-V2\x27\x0aitem1.TEL;waid=','https://www.guruapi.tech/K.jpg','6850QapAYH'];_0x2a71=function(){return _0x4a4035;};return _0x2a71();}import _0xbf8510 from'performance-now';let handler=async(_0x522480,{conn:_0x56505d})=>{const _0x3f3d34=_0x477f;let _0x86fc1=_0x3f3d34(0x111),_0x9edb5f={'key':{'fromMe':![],'participant':_0x3f3d34(0x101),'remoteJid':'status@broadcast'},'message':{'contactMessage':{'displayName':_0x3f3d34(0x116),'vcard':_0x3f3d34(0x110)+_0x522480[_0x3f3d34(0x115)][_0x3f3d34(0xff)]('@')[0x0]+':'+_0x522480['sender'][_0x3f3d34(0xff)]('@')[0x0]+_0x3f3d34(0x118)}}},_0x40c367=await _0x56505d['sendMessage'](_0x522480['chat'],{'text':'Pinging...'},{'quoted':_0x9edb5f}),_0x567650=_0xbf8510();await exec(_0x3f3d34(0x109),async(_0x3af436,_0x3214b5)=>{const _0x59e59a=_0x3f3d34;let _0x34dddc=(_0xbf8510()-_0x567650)[_0x59e59a(0x10a)](0x4);await _0x56505d['relayMessage'](_0x522480[_0x59e59a(0x10f)],{'protocolMessage':{'key':_0x40c367[_0x59e59a(0x11a)],'type':0xe,'editedMessage':{'conversation':_0x59e59a(0x104)+_0x34dddc+_0x59e59a(0xfd)}}},{});});};function _0x477f(_0x430002,_0x3aa0c1){const _0x2a7132=_0x2a71();return _0x477f=function(_0x477f3f,_0x844502){_0x477f3f=_0x477f3f-0xfc;let _0x32f551=_0x2a7132[_0x477f3f];return _0x32f551;},_0x477f(_0x430002,_0x3aa0c1);}handler[_0x422390(0x108)]=[_0x422390(0x10b)],handler[_0x422390(0x103)]=[_0x422390(0x117)],handler[_0x422390(0x10c)]=[_0x422390(0x10b),_0x422390(0x10e)];export default handler;
+/**
+ Copyright (C) 2022.
+ Licensed under the  GPL-3.0 License;
+ You may not use this file except in compliance with the License.
+ It is supplied in the hope that it may be useful.
+ * @project_name : Secktor-Md
+ * @author : SamPandey001 <https://github.com/SamPandey001>
+ * @description : Secktor,A Multi-functional whatsapp bot.
+ * @version 0.0.6
+ **/
+
+//---------------------------------------------------------------------------
+const Secktor = require('../lib')
+Secktor.cmd({
+        pattern: "ping",
+        desc: "To check ping",
+        category: "general",
+        filename: __filename,
+    },
+    async(Void, citel) => {
+        var inital = new Date().getTime();
+        const { key } = await Void.sendMessage(citel.chat, {text: '```𝐒𝐓𝐀𝐑 𝐈𝐒 𝐏𝐢𝐧𝐠𝐢𝐧𝐠!!!```'});
+        var final = new Date().getTime();
+       // await Secktor.sleep(1000)
+       return await Void.sendMessage(citel.chat, {text: '*𝐏𝐨𝐧𝐠 🏓*\n *' + (final - inital) + ' ms* ', edit: key});
+    }
+);
