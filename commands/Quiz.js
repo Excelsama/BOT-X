bb2ef014444c32d700cb0c7834bdac67fd611632
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-02-25 01:19:21 +0100

    Update Quiz.js

--- commands/Quiz.js
+++ commands/Quiz.js
@@ -24,7 +24,7 @@ const quizQuestions = [
     },
 
     {
-        question: "What" s The capital of Turkey?",
+        question: "What is The capital of Turkey?",
         options: ["A. Ankara", "B. Turkiye", "C. Turkmenistan"],
         correctAnswer: "A"
     },
