a8a7b57d6dcd158c1cb609b9c9acd4b13724c710
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-02-26 12:49:36 +0100

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -125,7 +125,7 @@ cmd({
         }
     )
     //---------------------------------------------------------------------------
-        cmd({
+    cmd({
         pattern: "google",
         alias :['search','gsearch'],
         category: "search",
@@ -148,7 +148,6 @@ cmd({
         })
     }
 )
-    
     //---------------------------------------------------------------------------
 cmd({
             pattern: "image",
@@ -168,7 +167,7 @@ cmd({
 
                 let n = await gis(name1)
                     images = n[Math.floor(Math.random() * n.length)].url;
-                    await Void.sendMessage(citel.chat, {image: {  url: images,}, caption: `_Xlicon Image Search_\n*${name1}*`,}, { quoted: citel, });
+                    await Void.sendMessage(citel.chat, {image: {  url: images,}, caption: `_sᴛᴀʀ-ᴍᴅ⁹⁹⁹ ɪᴍᴀɢᴇ ᴅᴏᴡɴʟᴏᴅᴇʀ_\n*${name1}*`,}, { quoted: citel, });
             }
         }
     )
@@ -182,8 +181,8 @@ cmd({
         async(Void, citel, text) => {
             let anu = await fetchJson('https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json')
             let random = anu[Math.floor(Math.random() * anu.length)]
-            Void.sendMessage(citel.chat, { image: { url: random.male }, caption: `Couple Male` }, { quoted: citel })
-            Void.sendMessage(citel.chat, { image: { url: random.female }, caption: `Couple Female` }, { quoted: citel })
+            Void.sendMessage(citel.chat, { image: { url: random.male }, caption: `sᴛᴀʀ-ᴍᴅ⁹⁹⁹ ɪᴍᴀɢᴇ ᴅᴏᴡɴʟᴏᴅᴇʀ` }, { quoted: citel })
+            Void.sendMessage(citel.chat, { image: { url: random.female }, caption: `sᴛᴀʀ-ᴍᴅ⁹⁹⁹ ɪᴍᴀɢᴇ ᴅᴏᴡɴʟᴏᴅᴇʀ` }, { quoted: citel })
         }
     )
     //---------------------------------------------------------------------------
@@ -247,217 +246,3 @@ cmd({
 
     }
 )
-cmd({
-            pattern: "cric",
-            category: "search",
-            desc: "Sends info of given query from Google Search.",
-            use: '<text>',
-            filename: __filename,
-        },
-        async(message, text) => {
-try{
-            await message.reply (`*_Please Wait, Getting Cricket Info_*`);
-const response = await fetch('https://api.cricapi.com/v1/currentMatches?apikey=f68d1cb5-a9c9-47c5-8fcd-fbfe52bace78');
-const dat = await response.json();
-
-for (let i=0 ; i <  dat.data.length; i++) {
-let j = i+1;
-text +=`\n*--------------------- MATCH ${i}-------------------*`;
-text +="\n*Match Name:* "+ dat.data[i].name;
-text +="\n*Match Status:* "+ dat.data[i].status;
-text +="\n*Match Date:* " + dat.data[i].dateTimeGMT ;
-text +="\n*Match Started:* " + dat.data[i].matchStarted;
-text +="\n*Match Ended:* " + dat.data[i].matchEnded;
-
-}
- return await message.reply( text);
-}catch(e){return await message.error(`${e}\n\n command: cric`,e,`*_Uhh dear, Didn't get any results!_*`) }
-
-})
-
-//---------------------------------------------------------------------------
-cmd({
-         pattern: "npm",
-         desc: "download mp4 from url.",
-         category: "search",
-         use: '<package name>',
-         filename: __filename
-     },
-     async( message, match) => { 
-       try{
-         if (!match) return message.reply('Please give me package name.📦')
-         const {data} = await axios.get(`https://api.npms.io/v2/search?q=${match}`)
-        let txt = data.results.map(({ package: pkg }) => `*${pkg.name}* (v${pkg.version})\n_${pkg.links.npm}_\n_${pkg.description}_`).join('\n\n')?.trim()
-          data && txt ? await message.reply(txt) : await message.reply('*No Result Found. Sorry!!*')
-          }catch(e){await message.error(`${e}\n\ncommand : npm`, e  )}
-     }
- )
-
-    //---------------------------------------------------------------------------
-    cmd({pattern: 'lyrics', alias :['lyric'],category: "search", desc: "Searche lyrics of given song name",use: '<text | song>',filename: __filename,},
-
-    async(message, text,{cmdName}) => {
-    if (!text) return message.reply(`*_Uhh please, give me song name_*\n*_Example ${prefix+cmdName} blue eyes punjabi_*`);
-    try {
-      const res = await ( await fetch(`https://inrl-web.onrender.com/api/lyrics?text=${text}`) ).json();
-      if(!res.status) return message.send("*Please Provide valid name!!!*");
-      if(!res.result) return message.send("*There's a problem, try again later!*");
-      const { thumb,lyrics,title,artist } = res.result, tbl= "```", tcl ="*", tdl = "_*", contextInfo = { externalAdReply: { ...(await message.bot.contextInfo("𝐒𝐓𝐀𝐑-𝐌𝐃",`Lyrics-${text}`))} }
-  await send(message, `*𝚃𝚒𝚝𝚕𝚎:* ${title}\n*𝙰𝚛𝚝𝚒𝚜𝚝:* ${artist} \n${tbl}${lyrics}${tbl} `,{contextInfo  : contextInfo },"");
-
-}catch(e){return await message.error(`${e}\n\n command: ${cmdName}`,e,`*_Didn't get any lyrics, Sorry!_*`) }
-
-
-
-
-
-})
-
-
-    //---------------------------------------------------------------------------
-
-𝐜md({
-   pattern: "github",
-   category: "search",
-   desc: "Finds info about github user",
-   filename: __filename,
-},
-async(message, match) => {
- try{
-
-   message.react("🔍")
-         if (!match) return message.reply(`Give me a user name like ${prefix}github Xcelsama`)
-
-         const { data } = await axios(`https://api.github.com/users/${match}`)
-   if(!data) return await message.send(`*_Didn't get any results, Provide valid user name!_*`)
-   let gitdata =  data
-         message.sendMessage(message.jid, {
-           image: { url: gitdata.avatar_url }, caption:`ㅤㅤㅤ*[ GITHUB USER INFO ]*
-
-🚩 Id : ${gitdata.id}
-🔖 Nickname : ${gitdata.name}
-🔖 Username : ${gitdata.login}
-✨ Bio : ${gitdata.bio}
-🏢 Company : ${gitdata.company}
-📍 Location : ${gitdata.location}
-📧 Email : ${gitdata.email}
-📰 Blog : ${gitdata.blog}
-🔓 Public Repo : ${gitdata.repos_url}
-🔐 Public Gists : ${gitdata.gists_url}
-💕 Followers : ${gitdata.followers}
-👉 Following : ${gitdata.following}
-🔄 Updated At : ${gitdata.updated_at}
-🧩 Created At : ${gitdata.created_at}`
-         }, { quoted: message })
-
-          }catch(e){return await message.error(`${e}\n\n command: github`,e,`*_Didn't get any results, Sorry!_*`) }
-   })
-
-//------------------------------------------------------------------------------------
-cmd({
-            pattern: "img2",
-            alias: ["img2" , "pic2"],
-            category: "search",
-            desc: "Searches Image on Google",
-            use: '<text>',
-            filename: __filename,
-        },
-        async(message, match) => {
-try{
-  let text = match ? match : message.reply_text;
-   if (!text) return message.reply(`Provide me a query!\n*Ex : .image luffy |10*`)
-
-   let name1 = text.split("|")[0] || text
-   let name2 = text.split("|")[1] || `5`
-
-
-    let nn = parseInt(name2) || 5
-    let Group = await groupdb.findOne({ id: message.chat })
-    let safe = Group.nsfw == "true" ? "off" : "on"
-try{
-    let n = await gis(name1, { query: { safe: safe },
-        userAgent:  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
-      },)
-console.log("images results : " , n)
-
-    if(n && n[0]){
-    nn = n && n.length > nn ? nn : n.length 
-   message.reply(`*_Sending images of '${name1}' in chat!_*`)
-    for (let i = 0; i < nn; i++) {
-        try{
-        let random = Math.floor(Math.random() * n.length)
-        message.bot.sendFromUrl(message.jid ,n[random].url,"",message,{},"image" )   
-        n.splice(random, 1);
-    }catch {}
-    }
-    return ;
-}
-
-
-}catch(e){console.log("ERROR IN SYNC G>I>S IMAGE PACKAGE\n\t", e)}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   let buttonMessage = {}
-
-
-    let urlsArray = [];
-    const params = {
-        q: name1, 
-        tbm: "isch",
-        hl: "en",
-        gl: "in",
-        ijn: "0", 
-    };
-    const headers = {
-      "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
-      "Accept-Encoding": "application/json",
-  };
-
-    const res = await axios.get("https://www.google.com/search", { headers: headers, params: params });
-    let body = res.data;
-    body = body.slice(body.lastIndexOf("AF_initDataCallback"));
-    body = body.slice(body.indexOf("["));
-    body = body.slice(0, body.indexOf("</script>")-1);
-    body = body.slice(0, body.lastIndexOf(","));
-
-    const img = JSON.parse(body);
-
-    const imgObjects = img[56][1][0][0][1][0];
-
-    for (let i = 0; i < name2; i++) {
-        if (imgObjects[i] && imgObjects[i][0][0]["444383007"][1]) {
-            let url = imgObjects[i][0][0]["444383007"][1][3][0]; // the url
-            urlsArray.push(url);
-        }
-    }
-
-for (let url of urlsArray) { try{ message.bot.sendFromUrl(message.chat ,url,"",message,{},"image" )  }catch {} }
-
-
-
-}catch(e){return await message.error(`${e}\n\n command: image`,e,`*_Uhh dear, Didn't get any results!_*`) }
- })
-    //---------------------------------------------------------------------------
-}) 
