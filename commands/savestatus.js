df5e45721f199cd355d232ee7619da6fd7f8fc7b
Excel Amadi <144502732+Xcelsama@users.noreply.github.com>
2024-02-28 03:00:06 +0100

    Update savestatus.js

--- commands/savestatus.js
+++ commands/savestatus.js
@@ -1,5 +1,14 @@
-/**Copy and give credits
-
+/**
+ Copyright (C) 2022.
+ Licensed under the  GPL-3.0 License;
+ You may not use this file except in compliance with the License.
+ It is supplied in the hope that it may be useful.
+ * @project_name : STAR-MD
+ * @author : https://github.com/xcelsama<Excel>
+ * @description : STAR,A Multi-functional whatsapp bot.
+ * @version 0.0.6
+ **/
+//---------------------------------------------------------------------------
 cmd({
   pattern: "save",
   desc: "Save status.",
