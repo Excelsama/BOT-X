a30e2378c40c02b4ff6751bfead3f77df1af696c
Excel Hepzibah Amadi <exbots99@gmail.com>
2024-10-16 02:27:45 +0100

    BOT-X

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const fs = require('fs').promises;
 const path = require('path');
 const config = require('./config');
-const connect = require('./lib/connection');
 const { getandRequirePlugins } = require('./db');
 
-global.__basedir = __dirname; // Set the base directory for the project
-
-const readAndRequireFiles = async (directory) => {
+async function readAndRequireFiles(directory) {
  try {
   const files = await fs.readdir(directory);
   return Promise.all(files.filter((file) => path.extname(file).toLowerCase() === '.js').map((file) => require(path.join(directory, file))));
@@ -14,24 +11,26 @@ const readAndRequireFiles = async (directory) => {
   console.error('Error reading and requiring files:', error);
   throw error;
  }
-};
+}
 
 async function initialize() {
  console.log('X-Asena');
  try {
   await readAndRequireFiles(path.join(__dirname, '/db/'));
   console.log('Syncing Database');
-
   await config.DATABASE.sync();
 
   console.log('⬇  Installing Plugins...');
   await readAndRequireFiles(path.join(__dirname, '/source/'));
   await getandRequirePlugins();
-  console.log('✅ Plugins Sucessfully Installed! In BOT-X');
-  return await connect();
+  console.log('✅ Plugins Installed!');
+
+  const WhatsAppBot = require('./lib/bot');
+  const bot = new WhatsAppBot();
+  return bot.connect();
  } catch (error) {
   console.error('Initialization error:', error);
-  return process.exit(1); // Exit with error status
+  process.exit(1);
  }
 }
 
