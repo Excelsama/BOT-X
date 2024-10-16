9b5fdd625ca3fb192d484e762c4566d252182cc0
Excel Hepzibah Amadi <exbots99@gmail.com>
2024-10-16 02:26:56 +0100

    BOT-X

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
-
-const toBool = (x) => x === 'true';
 const env = (key, def) => process.env[key] || def;
 const DB_URL = env('DATABASE_URL', './database.db');
 
@@ -25,15 +23,16 @@ if (DB_URL === './database.db' || !DB_URL.startsWith('postgres://')) {
 }
 
 module.exports = {
- LOGS: toBool(process.env.LOGS || 'true'),
+ LOGS: process.env.LOGS || true,
  SESSION_ID: process.env.SESSION_ID || '',
- PREFIX: process.env.HANDLER || '*',
+ PREFIX: process.env.HANDLER || '.',
+ ANTI_CALL: process.env.ANTI_CALL || 'false', // 'true' | 'false' | 'block'
  WARN_COUNT: 3,
- SUDO: process.env.SUDO || '2347045035241',
- AUTO_READ: toBool(process.env.AUTO_READ || 'false'),
- AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ || 'false'),
+ SUDO: process.env.SUDO || '',
+ AUTO_READ_MESSAGE: process.env.AUTO_READ || false,
+ AUTO_STATUS_READ: process.env.AUTO_STATUS_READ || true,
+ PRESENCE_UPDATE: process.env.PRESENCE_UPDATE || 'recording', // 'available' | 'unavailable' | 'composing'  |'recording' | 'paused'
  WORK_TYPE: process.env.WORK_TYPE || 'private',
- ANTI_DELETE: process.env.ANTI_DELETE || true,
  DATABASE_URL: DB_URL,
  DATABASE: sequelize,
 };
