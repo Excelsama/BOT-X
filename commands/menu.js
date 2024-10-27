8437ad95a530d97cbaff0511cebb65aaf7f06e6d
Excel Hepzibah Amadi <exbots99@gmail.com>
2024-10-27 18:34:06 +0100

    BOT-X

--- commands/menu.js
+++ commands/menu.js
@@ -61,7 +61,7 @@ Secktor.cmd({
                 for (const category in cmds) 
                 {
                    str += `╭────⋉ *${tiny(category)}* ⋊\n` ;
-                   if(text.toLowerCase() == category.toLowerCase()){ str = `╭─────❏ *${tiny(category)}* ❏\n` ;      
+                   if(text.toLowerCase() == category.toLowerCase()){ str = `╭─────⋉ *${tiny(category)}* ⋊\n` ;      
                         for (const plugins of cmds[category]) { str += `│ ${fancytext(plugins,1)}\n` ; }
                         str += `╰━━━━━━━━━━━──⪩\n`  ;
                         break ;
