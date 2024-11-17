5ea2d0ec01003aa351d83568a8379eec87e17fd6
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-11-17 09:36:04 +0100

    BOT-X

---
+++ index.js
@@ -0,0 +1,16 @@
+const bot = require(__dirname + "/lib/amd");
+const {
+  VERSION
+} = require(__dirname + "/config");
+const start = async () => {
+  Debug.info("Starting BOT-X " + VERSION);
+  try {
+    await bot.init();
+    await bot.DATABASE.sync();
+    await bot.connect();
+  } catch (_0x11dc77) {
+    Debug.error(_0x11dc77);
+    start();
+  }
+};
+start();
