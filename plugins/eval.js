5903309f2fbd430602a41651df76311b1ec75f99
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-12-01 00:47:05 +0100

    Update plugins

---
+++ plugins/eval.js
@@ -0,0 +1,56 @@
+
+const {
+  Function,
+  isPrivate,
+  getUrl,
+  fromBuffer,
+  Imgur,
+  getBuffer,
+  getJson,
+  Fancy,
+  AddMp3Meta,
+  createMap,
+  formatBytes,
+  parseJid,
+  isUrl,
+  parsedJid,
+  pinterest,
+  wallpaper,
+  wikimedia,
+  quotesAnime,
+  aiovideodl,
+  umma,
+  ringtone,
+  styletext,
+  FileSize,
+  h2k,
+  textpro,
+  yt,
+  ytIdRegex,
+  yta,
+  ytv,
+  runtime,
+  clockString,
+  sleep,
+  jsonformat,
+  Serialize,
+  processTime,
+  command,
+} = require("../lib/");
+const util = require("util");
+const config = require("../config");
+
+
+
+command({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
+  if (match.startsWith("$")) {
+    //const m = message;
+    try {
+      let evaled = await eval(`${match.replace(">", "")}`);
+      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+      await message.reply(evaled);
+    } catch (err) {
+      await message.reply(util.format(err));
+    }
+  }
+});
