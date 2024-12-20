ff1367b10fee6c491d5a48319b0d595036004634
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-12-20 21:30:16 +0100

    BOT-X

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,184 +1,112 @@
 
 const os = require("os");
 const Config = require("../config");
-let { fancytext, tiny, runtime, formatp, prefix} = require("../lib");
+let { fancytext, tiny, runtime, formatp, prefix } = require("../lib");
 const long = String.fromCharCode(8206);
 const readmore = long.repeat(4001);
 const astro_patch = require("../lib/plugins");
 const trend_usage = (() => {
     const trendNumber = ((min, max) => {
-      const random = () => Math.random();
-      const floor = (x) => Math.floor(x);
-      const multiply = (a, b) => a * b;
-      const add = (a, b) => a + b;
-      const subtract = (a, b) => a - b;
-      const randomValue = multiply(random(), subtract(max, min + 1));
-      const result = add(floor(randomValue), min);
-      return result;
+        const random = () => Math.random();
+        const floor = (x) => Math.floor(x);
+        const multiply = (a, b) => a * b;
+        const add = (a, b) => a + b;
+        const subtract = (a, b) => a - b;
+        const randomValue = multiply(random(), subtract(max, min + 1));
+        const result = add(floor(randomValue), min);
+        return result;
     })(1, 99);
     return trendNumber;
-  })();
+})();
 
-  const database_info = (() => {
+const database_info = (() => {
     const dbNumber = ((min, max) => {
-      const random = () => Math.random();
-      const floor = (x) => Math.floor(x);
-      const multiply = (a, b) => a * b;
-      const add = (a, b) => a + b;
-      const subtract = (a, b) => a - b;
-      const randomValue = multiply(random(), subtract(max, min + 1));
-      const result = add(floor(randomValue), min);
-      return result;
+        const random = () => Math.random();
+        const floor = (x) => Math.floor(x);
+        const multiply = (a, b) => a * b;
+        const add = (a, b) => a + b;
+        const subtract = (a, b) => a - b;
+        const randomValue = multiply(random(), subtract(max, min + 1));
+        const result = add(floor(randomValue), min);
+        return result;
     })(1, 499);
     return dbNumber;
-  })();
+})();
 
-astro_patch.smd(
-  {
-    cmdname: "menu",
-    desc: "Help list",
-    react: "💫",
-    desc: "To show all avaiable commands.",
-    type: "user",
-    filename: __filename,
-  },
-  async (message, input) => {
+astro_patch.smd({
+    cmdname: "lmenu",
+    react: '🍁',
+    desc: "To show all available commands.",
+    type: 'user',
+    filename: __filename
+}, async (context, message) => {
     try {
-      const { commands } = require("../lib");
-      if (input.split(" ")[0]) {
-        let commandDetails = [];
-        const foundCommand = commands.find(
-          (cmd) => cmd.pattern === input.split(" ")[0].toLowerCase(),
-        );
-        if (foundCommand) {
-          commandDetails.push("*🔉Command:* " + foundCommand.pattern);
-          if (foundCommand.category) {
-            commandDetails.push("*💁Category:* " + foundCommand.category);
-          }
-          if (foundCommand.alias && foundCommand.alias[0]) {
-            commandDetails.push("*💁Alias:* " + foundCommand.alias.join(", "));
-          }
-          if (foundCommand.desc) {
-            commandDetails.push("*💁Description:* " + foundCommand.desc);
-          }
-          if (foundCommand.use) {
-            commandDetails.push(
-              "*〽️Usage:*\n ```" +
-                prefix +
-                foundCommand.pattern +
-                " " +
-                foundCommand.use +
-                "```",
-            );
-          }
-          if (foundCommand.usage) {
-            commandDetails.push(
-              "*〽️Usage:*\n ```" + foundCommand.usage + "```",
-            );
-          }
-          await message.reply(commandDetails.join("\n"));
-        }
-      }
+        const { commands } = require("../lib");
+        const currentTime = new Date();
+        const hours = currentTime.getHours();
+        const minutes = currentTime.getMinutes();
+        const formattedTime = `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+        const currentDate = currentTime.toLocaleDateString();
+        let greeting = "";
 
-      let menuThemeType;
-      let menuThemeHeader;
-      let menuThemeFooter;
-      let menuThemeCategoryHeader;
-      let menuThemeCategoryFooter;
-      let menuThemeCommandPrefix;
-      let menuThemeCommandFooter;
-
-      if (Config.menu === "") {
-        menuThemeType = Math.floor(Math.random() * 4) + 1;
-      }
-
-      if (
-        menuThemeType === 1 ||
-        Config.menu.trim().startsWith("1") ||
-        Config.menu.toLowerCase().includes("menu1")
-      ) {
-        menuThemeHeader = "┏﹝ *" + Config.botname + "* ﹞";
-        menuThemeCommandPrefix = "┃ ✗";
-        menuThemeFooter = "┗═════════════〤";
-        menuThemeCategoryHeader = "┌『";
-        menuThemeCategoryFooter = "』";
-        menuThemeCommandPrefix = " | ";
-        menuThemeCommandFooter = "\n└═════════════〤";
-      } else if (
-        menuThemeType === 2 ||
-        Config.menu.trim().startsWith("2") ||
-        Config.menu.toLowerCase().includes("menu2")
-      ) {
-        menuThemeHeader = "╭────《 love*" + Config.botname + "*》────⪩";
-        menuThemeCommandPrefix = "𖣎│▸";
-        menuThemeFooter = "╰──────────⪩";
-        menuThemeCategoryHeader = "┌〈";
-        menuThemeCategoryFooter = "》──────⪩";
-        menuThemeCommandPrefix = "𖣎│▸ ";
-        menuThemeCommandFooter = "╰───────────⪩";
-      } else {
-        menuThemeHeader = "╭〘  " + Config.botname + "  〙";
-        menuThemeCommandPrefix = "│ │";
-        menuThemeFooter = "╰═══════════════⊷";
-        menuThemeCategoryHeader = "╭─❏";
-        menuThemeCategoryFooter = "❏";
-        menuThemeCommandPrefix = "│";
-        menuThemeCommandFooter = "╰════════════─⊷";
-      }
-
-      const categorizedCommands = {};
-      commands.map(async (command, index) => {
-        if (
-          command.dontAddCommandList === false &&
-          command.pattern !== undefined
-        ) {
-          if (!categorizedCommands[command.category]) {
-            categorizedCommands[command.category] = [];
-          }
-          categorizedCommands[command.category].push(command.pattern);
-        }
-      });
-
-      const currentTime = message.time;
-      const currentDate = message.date;
-      let menuText = `
-  ${menuThemeHeader}
-  ${menuThemeCommandPrefix} *ᴏᴡɴᴇʀ:* ${Config.ownername}
-  ${menuThemeCommandPrefix} *ᴜᴘᴛɪᴍᴇ:* ${runtime(process.uptime())}
-  ${menuThemeCommandPrefix} *ʀᴀᴍ ᴜsᴀɢᴇ:* ${formatp(os.totalmem() - os.freemem())}
-  ${menuThemeCommandPrefix} *ᴛɪᴍᴇ:* ${currentTime}
-  ${menuThemeCommandPrefix} *ᴅᴀᴛᴇ:* ${currentDate}
-  ${menuThemeCommandPrefix} *ᴄᴏᴍᴍᴀɴᴅs:* ${commands.length}
-  ${menuThemeCommandPrefix} *ᴜsᴇʀs:* ${trend_usage}
-  ${menuThemeCommandPrefix} *ᴅᴀᴛᴀʙᴀsᴇ:* ${database_info}
-  ${menuThemeFooter}\n                         
-  \n${readmore}\n`;
-
-      for (const category in categorizedCommands) {
-        menuText += `${menuThemeCategoryHeader} *${tiny(category)}* ${menuThemeCategoryFooter}\n`;
-        if (input.toLowerCase() === category.toLowerCase()) {
-          menuText = `${menuThemeCategoryHeader} *${tiny(category)}* ${menuThemeCategoryFooter}\n`;
-          for (const command of categorizedCommands[category]) {
-            menuText += `${menuThemeCommandPrefix} ${fancytext(command, 1)}\n`;
-          }
-          menuText += `${menuThemeCommandFooter}\n`;
-          break;
+        if (hours >= 5 && hours < 12) {
+            greeting = "Good day!";
+        } else if (hours >= 12 && hours < 18) {
+            greeting = "Good day!";
+        } else if (hours >= 18 && hours < 22) {
+            greeting = "Good Evening!";
         } else {
-          for (const command of categorizedCommands[category]) {
-            menuText += `${menuThemeCommandPrefix} ${fancytext(command, 1)}\n`;
-          }
-          menuText += `${menuThemeCommandFooter}\n`;
+            greeting = "Good evening!";
         }
-      }
-      menuText += Config.caption;
 
-      const messageOptions = {
-        caption: menuText,
-        ephemeralExpiration: 3000,
-      };
-      return await message.sendUi(message.chat, messageOptions, message);
+        const commandCategories = {};
+        commands.forEach(cmd => {
+            if (!cmd.dontAddCommandList && cmd.pattern) {
+                if (!commandCategories[cmd.category]) {
+                    commandCategories[cmd.category] = [];
+                }
+                commandCategories[cmd.category].push(cmd.pattern);
+            }
+        });
+
+        let menuThemeHeader = "╭────《 *" + Config.botname + "*》────⪩";
+        let menuThemeFooter = "╰──────────⪩";
+        let menuThemeCategoryHeader = "┌〈";
+        let menuThemeCategoryFooter = "》──────⪩";
+        let menuThemeCommandPrefix = "𖣎│▸ ";
+        let menuThemeCommandFooter = "╰───────────⪩";
+
+        let menuContent = `${menuThemeHeader}\n`;
+        menuContent += `𖣎│▸ *ᴏᴡɴᴇʀ:* ${Config.ownername}\n`;
+        menuContent += `𖣎│▸ *ᴜᴘᴛɪᴍᴇ:* ${runtime(process.uptime())}\n`;
+        menuContent += `𖣎│▸ *ʀᴀᴍ ᴜsᴀɢᴇ:* ${formatp(os.totalmem() - os.freemem())}\n`;
+        menuContent +=  `${menuThemeCommandPrefix}*ᴛɪᴍᴇ:* ${formattedTime}\n`;
+        menuContent += `𖣎│▸ *ᴅᴀᴛᴇ:* ${currentDate}\n`;
+        menuContent += `𖣎│▸ *ᴛᴏᴛᴀʟ ᴄᴍᴅ:* ${commands.length}\n`;
+        menuContent += `${menuThemeCommandPrefix}*ᴜsᴇʀs:* ${trend_usage}\n`;
+        menuContent += `${menuThemeCommandPrefix}*ᴅᴀᴛᴀʙᴀsᴇ:* ${database_info}\n`;
+        menuContent += `${menuThemeFooter}\n\n`;
+
+        menuContent += readmore + "\n\n";
+
+        for (const category in commandCategories) {
+            menuContent += `${menuThemeCategoryHeader} ${tiny(category)} ${menuThemeCategoryFooter}\n`;
+            commandCategories[category].forEach(cmd => {
+                menuContent += `${menuThemeCommandPrefix} ${fancytext(cmd, 1)}\n`;
+            });
+            menuContent += `${menuThemeCommandFooter}\n${readmore}\n\n`;
+        }
+
+        const finalMessage = menuContent + `©ʙᴏᴛ-x`;
+
+        await context.send(
+            "https://i.ibb.co/jTJDVYj/Leonardo-Phoenix-A-vibrant-animestyle-illustration-of-a-young-2.jpg", 
+            { caption: finalMessage },
+            "img",
+            message
+        );
+
     } catch (error) {
-      await message.error(error + "\nCommand: menu", error);
+        await context.error(error + "\nCommand: lmenu", error);
     }
-  },
-);
+});
