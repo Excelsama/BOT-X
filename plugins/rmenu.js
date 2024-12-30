d6aab9e4a52832f1494872232a53987521e4adb9
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-12-30 23:43:51 +0100

    Update rmenu.js

--- plugins/rmenu.js
+++ plugins/rmenu.js
@@ -1,184 +1,126 @@
-
-const os = require("os");
-const Config = require("../config");
-let { fancytext, tiny, runtime, formatp, prefix} = require("../lib");
+ const os = require('os');
+const fs = require("fs");
+const Config = require('../config');
+let {
+  fancytext,
+  tlang,
+  tiny,
+  bot_,
+  alive,
+  runtime,
+  formatp,
+  smsg,
+  getAdmin,
+  send,
+  react,
+  botpic,
+  sleep,
+  getBuffer,
+  prefix,
+  sck1,
+  smd,
+  sck,
+  getTime,
+  formatDate,
+  groupdb,
+  smdJson,
+  smdBuffer,
+  isAdmin
+} = require("../lib");
 const long = String.fromCharCode(8206);
 const readmore = long.repeat(4001);
-const astro_patch = require("../lib/plugins");
-const trend_usage = (() => {
-    const trendNumber = ((min, max) => {
-      const random = () => Math.random();
-      const floor = (x) => Math.floor(x);
-      const multiply = (a, b) => a * b;
-      const add = (a, b) => a + b;
-      const subtract = (a, b) => a - b;
-      const randomValue = multiply(random(), subtract(max, min + 1));
-      const result = add(floor(randomValue), min);
-      return result;
-    })(1, 99);
-    return trendNumber;
-  })();
+const xcel = require('../lib/plugins');
+const {
+  Sticker,
+  createSticker,
+  StickerTypes
+} = require("wa-sticker-formatter");
+const util = require("util");
+const {
+  commands
+} = require('../lib');
+const {
+  exec
+} = require("child_process");
+const translatte = require("translatte");
+const cron = require('node-cron');
+var cronStart = false;
 
-  const database_info = (() => {
-    const dbNumber = ((min, max) => {
-      const random = () => Math.random();
-      const floor = (x) => Math.floor(x);
-      const multiply = (a, b) => a * b;
-      const add = (a, b) => a + b;
-      const subtract = (a, b) => a - b;
-      const randomValue = multiply(random(), subtract(max, min + 1));
-      const result = add(floor(randomValue), min);
-      return result;
-    })(1, 499);
-    return dbNumber;
-  })();
-
-astro_patch.smd(
-  {
-    cmdname: "rmenu",
-    desc: "Help list 2",
-    react: "💫",
-    desc: "To show all avaiable commands.",
-    type: "user",
-    filename: __filename,
-  },
-  async (message, input) => {
-    try {
-      const { commands } = require("../lib");
-      if (input.split(" ")[0]) {
-        let commandDetails = [];
-        const foundCommand = commands.find(
-          (cmd) => cmd.pattern === input.split(" ")[0].toLowerCase(),
-        );
-        if (foundCommand) {
-          commandDetails.push("*🔉Command:* " + foundCommand.pattern);
-          if (foundCommand.category) {
-            commandDetails.push("*💁Category:* " + foundCommand.category);
-          }
-          if (foundCommand.alias && foundCommand.alias[0]) {
-            commandDetails.push("*💁Alias:* " + foundCommand.alias.join(", "));
-          }
-          if (foundCommand.desc) {
-            commandDetails.push("*💁Description:* " + foundCommand.desc);
-          }
-          if (foundCommand.use) {
-            commandDetails.push(
-              "*〽️Usage:*\n ```" +
-                prefix +
-                foundCommand.pattern +
-                " " +
-                foundCommand.use +
-                "```",
-            );
-          }
-          if (foundCommand.usage) {
-            commandDetails.push(
-              "*〽️Usage:*\n ```" + foundCommand.usage + "```",
-            );
-          }
-          await message.reply(commandDetails.join("\n"));
+xcel.smd({
+  'cmdname': "rmenu",
+  'desc': "Help list",
+  'type': "general",
+  'filename': __filename
+}, async (_0xd2266a, _0x54954a) => {
+  try {
+    const {
+      commands: _0x4f1da9
+    } = require("../lib");
+    if (_0x54954a.split(" ")[0x0]) {
+      let _0x35206d = [];
+      const _0xd17d83 = _0x4f1da9.find(_0x3cf852 => _0x3cf852.pattern === _0x54954a.split(" ")[0x0].toLowerCase());
+      if (_0xd17d83) {
+        _0x35206d.push("*🍁Command:* " + _0xd17d83.pattern);
+        if (_0xd17d83.category) {
+          _0x35206d.push("*🧩Category:* " + _0xd17d83.category);
         }
-      }
-
-      let menuThemeType;
-      let menuThemeHeader;
-      let menuThemeFooter;
-      let menuThemeCategoryHeader;
-      let menuThemeCategoryFooter;
-      let menuThemeCommandPrefix;
-      let menuThemeCommandFooter;
-
-      if (Config.menu === "") {
-        menuThemeType = Math.floor(Math.random() * 4) + 1;
-      }
-
-      if (
-        menuThemeType === 1 ||
-        Config.menu.trim().startsWith("1") ||
-        Config.menu.toLowerCase().includes("menu1")
-      ) {
-        menuThemeHeader = "┏﹝ *" + Config.botname + "* ﹞";
-        menuThemeCommandPrefix = "┃ ✗";
-        menuThemeFooter = "┗═════════════〤";
-        menuThemeCategoryHeader = "┌『";
-        menuThemeCategoryFooter = "』";
-        menuThemeCommandPrefix = " | ";
-        menuThemeCommandFooter = "\n└═════════════〤";
-      } else if (
-        menuThemeType === 2 ||
-        Config.menu.trim().startsWith("2") ||
-        Config.menu.toLowerCase().includes("menu2")
-      ) {
-        menuThemeHeader = "╭────《*" + Config.botname + "*》────⪩";
-        menuThemeCommandPrefix = "𖣎│▸";
-        menuThemeFooter = "╰──────────⪩";
-        menuThemeCategoryHeader = "┌〈";
-        menuThemeCategoryFooter = "》──────⪩";
-        menuThemeCommandPrefix = "𖣎│▸ ";
-        menuThemeCommandFooter = "╰───────────⪩";
-      } else {
-        menuThemeHeader = "╭〘  " + Config.botname + "  〙";
-        menuThemeCommandPrefix = "│ │";
-        menuThemeFooter = "╰═══════════════⊷";
-        menuThemeCategoryHeader = "╭─❏";
-        menuThemeCategoryFooter = "❏";
-        menuThemeCommandPrefix = "│";
-        menuThemeCommandFooter = "╰════════════─⊷";
-      }
-
-      const categorizedCommands = {};
-      commands.map(async (command, index) => {
-        if (
-          command.dontAddCommandList === false &&
-          command.pattern !== undefined
-        ) {
-          if (!categorizedCommands[command.category]) {
-            categorizedCommands[command.category] = [];
-          }
-          categorizedCommands[command.category].push(command.pattern);
+        if (_0xd17d83.alias && _0xd17d83.alias[0x0]) {
+          _0x35206d.push("*🧩Alias:* " + _0xd17d83.alias.join(", "));
         }
-      });
-
-      const currentTime = message.time;
-      const currentDate = message.date;
-      let menuText = `
-  ${menuThemeHeader}
-  ${menuThemeCommandPrefix} *ᴏᴡɴᴇʀ:* ${Config.ownername}
-  ${menuThemeCommandPrefix} *ᴜᴘᴛɪᴍᴇ:* ${runtime(process.uptime())}
-  ${menuThemeCommandPrefix} *ʀᴀᴍ ᴜsᴀɢᴇ:* ${formatp(os.totalmem() - os.freemem())}
-  ${menuThemeCommandPrefix} *ᴛɪᴍᴇ:* ${currentTime}
-  ${menuThemeCommandPrefix} *ᴅᴀᴛᴇ:* ${currentDate}
-  ${menuThemeCommandPrefix} *ᴄᴏᴍᴍᴀɴᴅs:* ${commands.length}
-  ${menuThemeCommandPrefix} *ᴜsᴇʀs:* ${trend_usage}
-  ${menuThemeCommandPrefix} *ᴅᴀᴛᴀʙᴀsᴇ:* ${database_info}
-  ${menuThemeFooter}\n                         
-  \n${readmore}\n`;
-
-      for (const category in categorizedCommands) {
-        menuText += `${menuThemeCategoryHeader} *${tiny(category)}* ${menuThemeCategoryFooter}\n`;
-        if (input.toLowerCase() === category.toLowerCase()) {
-          menuText = `${menuThemeCategoryHeader} *${tiny(category)}* ${menuThemeCategoryFooter}\n`;
-          for (const command of categorizedCommands[category]) {
-            menuText += `${menuThemeCommandPrefix} ${fancytext(command, 1)}\n`;
-          }
-          menuText += `${menuThemeCommandFooter}\n`;
-          break;
-        } else {
-          for (const command of categorizedCommands[category]) {
-            menuText += `${menuThemeCommandPrefix} ${fancytext(command, 1)}\n`;
-          }
-          menuText += `${menuThemeCommandFooter}\n`;
+        if (_0xd17d83.desc) {
+          _0x35206d.push("*🧩Description:* " + _0xd17d83.desc);
         }
+        if (_0xd17d83.use) {
+          _0x35206d.push("*〽️Usa:*\n ```" + prefix + _0xd17d83.pattern + " " + _0xd17d83.use + "```");
+        }
+        if (_0xd17d83.usage) {
+          _0x35206d.push("*〽️Usage:*\n ```" + _0xd17d83.usage + "```");
+        }
+        await _0xd2266a.reply(_0x35206d.join("\n"));
       }
-      menuText += Config.caption;
-
-      const messageOptions = {
-        caption: menuText,
-        ephemeralExpiration: 3000,
-      };
-      return await message.sendUi(message.chat, messageOptions, message);
-    } catch (error) {
-      await message.error(error + "\nCommand: rmenu", error);
     }
-  },
-);
+    let menuThemeHeader = "╭────《*" + Config.botname + "*》────⪩";
+    let menuThemeCommandPrefix = "𖣎│▸";
+    let menuThemeFooter = "╰──────────⪩";
+    let menuThemeCategoryHeader = "┌〈";
+    let menuThemeCategoryFooter = "》──────⪩";
+    let menuThemeCommandPrefix = "𖣎│▸ ";
+    let menuThemeCommandFooter = "╰───────────⪩";
+    
+    const _0x376e27 = {};
+    _0x4f1da9.map(async (_0x3b0442, _0x3e603e) => {
+      if (_0x3b0442.dontAddCommandList === false && _0x3b0442.pattern !== undefined) {
+        if (!_0x376e27[_0x3b0442.category]) {
+          _0x376e27[_0x3b0442.category] = [];
+        }
+        _0x376e27[_0x3b0442.category].push(_0x3b0442.pattern);
+      }
+    });
+    const _0x1b2e30 = _0xd2266a.time;
+    const _0x35bd69 = _0xd2266a.date;
+    let _0x192602 = menuThemeHeader + "\n" + menuThemeCommandPrefix + " Theme:- " + tlang().title + "\n" + menuThemeCommandPrefix + " Owner:- " + Config.ownername + "\n" + menuThemeCommandPrefix + " Plugins:- " + _0x4f1da9.length + "\n" + menuThemeCommandPrefix + " Uptime:- " + runtime(process.uptime()) + "\n" + menuThemeCommandPrefix + " Mem:- " + formatp(os.totalmem() - os.freemem()) + '/' + formatp(os.totalmem()) + "\n" + menuThemeCommandPrefix + " Time:- " + _0x1b2e30 + "\n" + menuThemeCommandPrefix + " Date:- " + _0x35bd69 + "\n" + menuThemeFooter + "\n\n";
+    for (const _0x2745af in _0x376e27) {
+      _0x192602 += menuThemeCategoryHeader + " *" + tiny(_0x2745af) + "* " + menuThemeCategoryFooter + "\n";
+      if (_0x54954a.toLowerCase() == _0x2745af.toLowerCase()) {
+        _0x192602 = menuThemeCategoryHeader + " *" + tiny(_0x2745af) + "* " + menuThemeCategoryFooter + "\n";
+        for (const _0x375619 of _0x376e27[_0x2745af]) {
+          _0x192602 += menuThemeCommandPrefix + " " + fancytext(_0x375619, 0x1) + "\n";
+        }
+        _0x192602 += menuThemeCommandFooter + "\n";
+        break;
+      } else {
+        for (const _0x3d11d4 of _0x376e27[_0x2745af]) {
+          _0x192602 += menuThemeCommandPrefix + " " + fancytext(_0x3d11d4, 0x1) + "\n";
+        }
+        _0x192602 += menuThemeCommandFooter + "\n";
+      }
+    }
+    _0x192602 += Config.caption;
+    let _0x2ca50e = {
+      'caption': _0x192602
+    };
+    return await _0xd2266a.sendUi(_0xd2266a.chat, _0x2ca50e, _0xd2266a);
+  } catch (_0x323e96) {
+    await _0xd2266a.error(_0x323e96 + "\nCommand:rmenu", _0x323e96);
+  }
+});
