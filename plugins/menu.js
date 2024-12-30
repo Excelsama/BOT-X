adec493fe50899184c96f3eefbd410748ee952c1
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-12-30 23:34:33 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -29,8 +29,7 @@ let {
 } = require("../lib");
 const long = String.fromCharCode(8206);
 const readmore = long.repeat(4001);
-const astro {};
-const astro.smd = require('../lib/plugins');
+const xcel = require('../lib/plugins');
 const {
   Sticker,
   createSticker,
@@ -54,7 +53,7 @@ let "USER_IMAGES" = {
       "required" :false
     },
 */
-astro.smd({
+xcel.smd({
   'cmdname': "menu",
   'desc': "Help list",
   'type': "general",
