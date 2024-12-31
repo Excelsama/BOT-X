56d32c770cc0648262ae0dfb39e55694e9f9f8da
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-12-31 07:51:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -45,7 +45,7 @@ global.SESSION_ID =
   process.env.SESSION_ID ||
   " " // session id here
 module.exports = {
-  menu: process.env.MENU || "2",
+  menu: process.env.MENU || "1",
   HANDLERS: process.env.PREFIX || "*",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
