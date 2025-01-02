cc2bb816fc96ca2f3ee7d492ff09b7e4db20169b
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2025-01-03 00:57:34 +0100

    Update eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -52,7 +52,7 @@ you may not use this file except in compliance with the License.
 X-Asena - X-Electra
 */
 
-command({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
+smd({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
   if (match.startsWith("$")) {
     //const m = message;
     try {
