69067737f5c2c8c8bcaa0e0e0add28d77c211614
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2025-01-03 00:57:19 +0100

    Update eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -1,103 +1,66 @@
+
+/* Copyright (C) 2022 X-Electra.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+X-Asena - X-Electra
+*/
+
 const {
-
   Function,
-
   isPrivate,
-
   getUrl,
-
   fromBuffer,
-
   Imgur,
-
   getBuffer,
-
   getJson,
-
   Fancy,
-
   AddMp3Meta,
-
   createMap,
-
   formatBytes,
-
   parseJid,
-
   isUrl,
-
   parsedJid,
-
   pinterest,
-
   wallpaper,
-
   wikimedia,
-
   quotesAnime,
-
   aiovideodl,
-
   umma,
-
   ringtone,
-
   styletext,
-
   FileSize,
-
   h2k,
-
   textpro,
-
   yt,
-
   ytIdRegex,
-
   yta,
-
   ytv,
-
   runtime,
-
   clockString,
-
   sleep,
-
   jsonformat,
-
   Serialize,
-
   processTime,
-
   smd,
-
 } = require("../lib/");
-
 const util = require("util");
-
 const config = require("../config");
 
-smd({pattern:'eval', on: "text", fromMe: true, desc: 'Runs a server code'}, async (message, match, m, client) => {
+/* Copyright (C) 2022 X-Electra.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+X-Asena - X-Electra
+*/
 
+command({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
   if (match.startsWith("$")) {
-
     //const m = message;
-
     try {
-
       let evaled = await eval(`${match.replace("$", "")}`);
-
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-
       await message.reply(evaled);
-
     } catch (err) {
-
       await message.reply(util.format(err));
-
     }
-
   }
-
 });
