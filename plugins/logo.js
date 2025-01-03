2a905a721252d0717ad80d6718fba8d56c77c74f
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-01-02 17:59:24 -0800

    Update logo.js

--- plugins/logo.js
+++ plugins/logo.js
@@ -26,19 +26,17 @@ smd(
 
       const response = await axios.get(apiUrl);
       const result = response.data;
-       console.log(result);
+      console.log(result);
 
       if (!result.success || !result.result || !result.result.image_url) {
         return await message.reply("Failed to generate the image. Please try again later.");
       }
       const imageUrl = result.result.image_url;
-      await message.send({
-        image: { url: imageUrl },
-        caption: `\n> Logo generated successfully.`,
-      });
+      const finalMessage = `\n> Logo generated successfully.`;
+      await message.send(imageUrl, { caption: finalMessage }, "img", message);
     } catch (error) {
       console.error("Image Generator Error:", error);
-      await message.error("An error occurred while generating the image:" ,error);
+      await message.error("An error occurred while generating the image:", error);
     }
   }
 );
